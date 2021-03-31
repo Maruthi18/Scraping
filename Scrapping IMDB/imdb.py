bf4818f21e2169a66783824f1f2bbcc232b00b60
Maruthi18 <syavemaruthi18@gmail.com>
2021-04-01 02:33:09 +0530

    here we generate top movie title link

---
+++ Scrapping IMDB/imdb.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get ('https://www.imdb.com/chart/top/')
+
+# print(res.status_code) # status code tell that whether we are getting the data are not
+
+html = res.text
+
+soup = BeautifulSoup (html, 'html.parser')
+
+tbody = soup.find ('tbody', { 'class': 'lister-list' })
+# print(table_body) # here we get everything inside the tbody tag
+
+trs = tbody.findAll ('tr')
+for tr in trs :
+	td = tr.find ('td', {'class': 'titleColumn'})
+	movie_id = td.a['href']
+	movie_url = f'https://www.imdb.com/{movie_id}'
+	print(movie_url)
+
